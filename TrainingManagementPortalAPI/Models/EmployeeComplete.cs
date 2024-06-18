24f798bebda92065eff8d087b216effcde9abf29
VT71 <56217276+VT71@users.noreply.github.com>
2024-06-18 12:30:07 +0300

    Added a new field for Employee Complete: DepartmentId

--- TrainingManagementPortalAPI/Models/EmployeeComplete.cs
+++ TrainingManagementPortalAPI/Models/EmployeeComplete.cs
@@ -8,6 +8,7 @@ namespace TrainingManagementPortalAPI
         public string Email { get; set; }
         public string FullName { get; set; }
         public string DepartmentName { get; set; }
+        public int DepartmentId { get; set; }
 
         public EmployeeComplete()
         {
