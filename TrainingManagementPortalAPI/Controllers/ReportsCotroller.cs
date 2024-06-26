af57e6d474d0c15216a2f7deba1b8f2ccd9dfa90
VT71 <56217276+VT71@users.noreply.github.com>
2024-06-26 16:19:17 +0300

    Created separate controller for reports

---
+++ TrainingManagementPortalAPI/Controllers/ReportsCotroller.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingManagementPortalAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class ReportsController : ControllerBase
+{
+
+    DataContextDapper _dapper;
+
+    public ReportsController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+
+    [HttpGet("GetPercentageOfCompletedTrainingsByRange")]
+    public decimal GetPercentageOfCompletedTrainingsByRange(string startDate, string endDate)
+    {
+        var allParameters = new
+        {
+            StartDate = startDate,
+            EndDate = endDate
+        };
+
+        string sql = @"SELECT COUNT(*)
+                        FROM TrainingDatabaseSchema.Trainings AS T
+                    WHERE T.Deadline >= @StartDate AND T.Deadline <= @EndDate";
+
+        decimal totalTrainings = _dapper.LoadDataSingle<decimal>(sql, allParameters);
+
+        var current = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+
+        var completedParameters = new
+        {
+            TodaysDate = current,
+            StartDate = startDate,
+            EndDate = endDate
+        };
+
+        sql = @"EXECUTE TrainingDatabaseSchema.GetCompletedTrainingsByRange @TodaysDate = @TodaysDate, @StartDate = @StartDate, @EndDate = @EndDate;";
+
+        decimal totalCompletedTrainings = _dapper.LoadDataSingle<decimal>(sql, completedParameters);
+
+
+        return Math.Round(totalCompletedTrainings / totalTrainings, 2);
+    }
+}
