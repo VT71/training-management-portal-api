85cd304f25b85d67844f0ec2ec38907894ad4ba9
VT71 <56217276+VT71@users.noreply.github.com>
2024-07-03 16:23:07 +0300

    Fixed nullable Trainer declaration

--- TrainingManagementPortalAPI/Models/TrainingsComplete.cs
+++ TrainingManagementPortalAPI/Models/TrainingsComplete.cs
@@ -8,11 +8,11 @@ namespace TrainingManagementPortalAPI
         public int Individual { get; set; }
         public string Adress { get; set; }
         public DateTime Deadline { get; set; }
-        public int ?Trainer { get; set; }
+        public int? Trainer { get; set; }
         public string TrainerName { get; set; }
         public int ForDepartments { get; set; }
         public int ForEmployees { get; set; }
-        public  IEnumerable<Department> Departments { get; set; }  // Schimbat de la int la List<int>
+        public IEnumerable<Department> Departments { get; set; }  // Schimbat de la int la List<int>
         public IEnumerable<EmployeeComplete> Employees { get; set; }
         public IEnumerable<Sections> Sections { get; set; }
     
