20e39c0d1717f02423be05385e46d92b18c5bb58
VT71 <56217276+VT71@users.noreply.github.com>
2024-07-04 11:27:38 +0300

    Added an ednpoint for updating the employee section progress, based on whether it already exists or a new one should be created

--- TrainingManagementPortalAPI/Controllers/ProgressController.cs
+++ TrainingManagementPortalAPI/Controllers/ProgressController.cs
@@ -30,4 +30,48 @@ public class ProgressController : ControllerBase
 
         return allProgress;
     }
+
+    [HttpPost("UpdateSectionProgress")]
+    public IActionResult UpdateSectionProgress(int sectionId, string userId, int progress)
+    {
+        string sql = "";
+
+        sql = @"SELECT E.EmployeeId FROM TrainingDatabaseSchema.Employees AS E 
+                                JOIN TrainingDatabaseSchema.Users AS U ON E.UserId = U.UserId 
+                            WHERE U.UserId = '" + userId + "'";
+
+        int userEmployeeId = _dapper.LoadDataSingle<int>(sql);
+
+        if (userEmployeeId > 0)
+        {
+            var parameters = new
+            {
+                SectionId = sectionId,
+                EmployeeId = userEmployeeId,
+                Progress = progress
+            };
+
+            sql = @"MERGE TrainingDatabaseSchema.Progress AS target
+                    USING (SELECT @SectionId AS SectionId, @EmployeeId AS EmployeeId) AS source
+                        ON (target.SectionId = source.SectionId AND target.EmployeeId = source.EmployeeId)
+                    WHEN MATCHED THEN
+                        UPDATE SET Progress = @Progress
+                    WHEN NOT MATCHED BY TARGET THEN
+                        INSERT (SectionId, EmployeeId, Progress)
+                        VALUES (@SectionId, @EmployeeId, 0);";
+
+            if (_dapper.ExecuteSql(sql, parameters) == true)
+            {
+                return Ok("Progress updated successfully");
+            }
+            else
+            {
+                return BadRequest("Progress not updated");
+            }
+        }
+        else
+        {
+            return BadRequest("User not found");
+        }
+    }
 }
